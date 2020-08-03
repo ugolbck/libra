d51349f81ffdf38e06946469063bd92129685e37
Pranav Teegavarapu <pranavnt@outlook.com>
2020-08-03 12:26:45 -0700

    added gajillion optional params

--- libra/query/nlp_queries.py
+++ libra/query/nlp_queries.py
@@ -101,6 +101,7 @@ def text_classification_query(self, instruction, drop=None,
                               save_path=os.getcwd()):
     """
     function to apply text_classification algorithm for sentiment analysis
+    :param generate_plots:
     :param many params: used to hyperparametrize the function.
     :return a dictionary object with all of the information for the algorithm.
     """
@@ -819,10 +820,32 @@ def get_ner(self, instruction):
     return self.models["named_entity_recognition"]
 
 
-def question_answering_query(learning_rate=3e-5, num_train_epochs=2, max_seq_length=384,
-                             doc_stride=128,
-                             overwrite_output_dir=True,
-                             reprocess_input_data=False,
-                             train_batch_size=2,
-                             gradient_accumulation_steps=8,
-                             )
+def question_answering_query(output_dir = 'outputs/',
+                             cache_dir = 'cache_dir/',
+                             fp16_opt_level = 'O1',
+                             max_seq_length = 512,
+                             train_batch_size = 8,
+                             gradient_accumulation_steps = 1,
+                             eval_batch_size = 8,
+                             num_train_epochs = 1,
+                             weight_decay = 0,
+                             learning_rate = 4e-5,
+                             adam_epsilon = 1e-8,
+                             warmup_ratio = 0.06,
+                             warmup_steps = 0,
+                             max_grad_norm = 1.0,
+                             logging_steps = 50,
+                             save_steps = 2000,
+                             evaluate_during_training = False,
+                             overwrite_output_dir = False,
+                             reprocess_input_data = False,
+                             process_count = cpu_count() - 2 if cpu_count() > 2 else 1,
+                             n_gpu = 1,
+                             silent = False,
+                             doc_stride = 384,
+                             max_query_length = 64,
+                             n_best_size = 20,
+                             max_answer_length = 100,
+                             null_score_diff_threshold=0.0):
+    pass
+
